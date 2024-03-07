f60a14a51192da5ec51aa80f89eb622ab3551b4a
Juan Marcano <marchanoth@protonmail.com>
2024-03-07 15:36:18 -0500

    proyecto actualizado

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import { WidgetItem } from "@/components/WidgetItem";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      {/* TODO: dashboard/page.tsx  */}
+          {/* Este contenido va dentro de page.tsx */}
+          <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+
+            {/* TODO: src/components <WidgetItem /> */}
+            <WidgetItem/>
+            {/* TODO: Fin <WidgetItem /> */}
+
+          </div>
+          {/* TODO: fin del dashboard/page.tsx  */}
+    </div>
+  );
+}
